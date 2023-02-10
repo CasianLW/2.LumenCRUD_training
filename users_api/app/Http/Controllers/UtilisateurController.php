08fdd461f7f22be13f4c9f57532fd5ad676e0f78
CasianLW <ciorbacasian@gmail.com>
2023-02-10 23:17:14 +0100

    methode store qui enregistre les nv users ainsi que la partie enregistrement photo profil

--- users_api/app/Http/Controllers/UtilisateurController.php
+++ users_api/app/Http/Controllers/UtilisateurController.php
@@ -11,8 +11,8 @@ class UtilisateurController extends Controller
     public function index()
     {
         // Get all data from db
-        $utiisateurs = Utilisateur::all();
-        return response()->json($utiisateurs);
+        $utilisateurs = Utilisateur::all();
+        return response()->json($utilisateurs);
     }
 
 
@@ -20,13 +20,45 @@ class UtilisateurController extends Controller
     public function store(Request $request)
     {
         //POST data to db from user (form)
+        // 1.Validation
+        $this->validate($request,[
+            'photo'=>'required',
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'username'=>'required',
+            'password'=>'required',
+        ]);
+        $utilisateur = new Utilisateur();
+
+        // 2. image upload
+        if($request->hasFile('photo')){
+            $file = $request->file('photo');
+            $allowedfileExtension = ['pdf','png','jpeg'];
+            $extension = $file->getClientOriginalExtension();
+            $check = in_array($extension, $allowedfileExtension);
+
+            if($check){
+                $name = time().$file->getClientOriginalName();
+                $file->move('images',$name);
+                $utilisateur->photo = $name;
+            }
+        }
+        // 3.text data
+        $utilisateur->first_name = $request->input('first_name');
+        $utilisateur->last_name = $request->input('last_name');
+        $utilisateur->username = $request->input('username');
+        $utilisateur->password = $request->input('password');
+
+        $utilisateur->save();
+
+        return response()->json($utilisateur);
     }
 
     public function show($id)
     {
         //give 1 user from users list
-        $utiisateur = Utilisateur::find($id);
-        return response()->json($utiisateur);
+        $utilisateur = Utilisateur::find($id);
+        return response()->json($utilisateur);
 
     }
 
@@ -43,8 +75,8 @@ class UtilisateurController extends Controller
     public function destroy($id)
     {
         //Delete user by ID
-        $utiisateur = Utilisateur::find($id);
-        $utiisateur->delete();
+        $utilisateur = Utilisateur::find($id);
+        $utilisateur->delete();
         return response()->json(`Utilisateur #$id supprimé avec succes !`);
 
     }
