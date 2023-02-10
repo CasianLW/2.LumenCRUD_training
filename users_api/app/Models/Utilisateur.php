0965e88051403caadab080eddc6bf48a5c4b5b0c
CasianLW <ciorbcasian@gmail.com>
2023-02-10 17:48:16 +0100

    model de l'utilisateur pour generer la table db

---
+++ users_api/app/Models/Utilisateur.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Utilisateur extends Model
+{
+    //
+    protected $fillable=[
+        'photo','first_name', 'last_name','username','email','password'
+    ];
+}
